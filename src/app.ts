867942d61a18601891b88d5129c36557ea89a684
rkathawate <rkathawate@sevone.com>
2021-09-18 15:49:05 +0530

    connect to db using mongoose

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,18 @@
-console.log("Hello there!!");
+import express from "express";
+import config from "config";
+import log from "./logger";
+import connect from "./db/connect";
+
+const host = config.get("host") as string;
+const port = config.get("port") as number;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.listen(port, host, ()=>{
+    log.info(`Server listening at http://${host}:${port}`);
+    // connect to db
+    connect();
+})

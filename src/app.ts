7dd0f853b9e87ca16414d4ad815e04d70afc1d7a
rkathawate <rkathawate@sevone.com>
2021-09-20 21:03:13 +0530

    update app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import config from "config";
 import log from "./logger";
 import connect from "./db/connect";
+import routes from "./routes";
 
 const host = config.get("host") as string;
 const port = config.get("port") as number;
@@ -15,4 +16,5 @@ app.listen(port, host, ()=>{
     log.info(`Server listening at http://${host}:${port}`);
     // connect to db
     connect();
+    routes(app);
 })
